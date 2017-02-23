3331a94391f13b11d1a6cc492b5707f4f2d61a35
Thuya Aung <thu.ya.a@gmail.com>
2017-02-23 21:59:11 +0800

    Initial Commit

---
+++ boomshakalaka/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  boomshakalaka
+//
+//  Created by Thuya Aung on 22/2/17.
+//  Copyright © 2017 Thuya Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnBoom: UIButton!
+    @IBOutlet weak var imgBg: UIImageView!
+    @IBOutlet weak var imgBoomApp: UIImageView!
+    @IBOutlet weak var foreground: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imgBg.isHidden=true
+        imgBoomApp.isHidden=true
+        foreground.isHidden=true
+        btnBoom.isHidden=false
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func touchBtnBoom(_ sender: UIButton) {
+        imgBg.isHidden=false
+        imgBoomApp.isHidden=false
+        foreground.isHidden=false
+        btnBoom.isHidden=true
+    }
+
+    @IBAction func btnForeground(_ sender: UIButton) {
+        imgBg.isHidden=true
+        imgBoomApp.isHidden=true
+        foreground.isHidden=true
+        btnBoom.isHidden=false
+    }
+}
+
